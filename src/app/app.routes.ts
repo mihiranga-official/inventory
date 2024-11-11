5347b573cf208290d0c7464548213b8fab180108
Janith-Mihiranga <janith.damroit@gmail.com>
2024-11-11 16:43:22 +0530

    status developed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { InvoiceComponent } from './invoice/invoice.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import { StatusComponent } from './status/status.component';
 
 
 export const routes: Routes = [
 
-  {path:'',component:ReservationComponent}
+  {path:'',component:StatusComponent}
 
 
 ];
